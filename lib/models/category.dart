f861231c3762f3fb4083be19db662f3f36d97c26
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2026-07-07 17:43:37 +0200

    Category grid

---
+++ lib/models/category.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  Category({required this.title, this.color = Colors.orange, required this.id});
+
+  final String id;
+  final String title;
+  final Color color;
+}
---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,39 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:resturang_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      //splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withAlpha(60),
+              category.color.withAlpha(225),
+            ],
+            begin: AlignmentGeometry.topStart,
+            end: AlignmentGeometry.bottomRight,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(
+            context,
+          ).textTheme.titleLarge!.copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      ),
+    );
+  }
+}

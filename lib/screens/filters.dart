0140584b96b2d542462d667374138eb9ca3a36e7
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2026-07-20 01:12:47 +0200

    more on navigation

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,18 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:resturang_app/screens/tabs.dart';
+import 'package:resturang_app/widgets/main_drawer.dart';
 
 class FilterScreen extends StatefulWidget {
-  const FilterScreen
-({super.key});
+  const FilterScreen({super.key});
 
   @override
-  State<FilterScreen
-> createState() => _FilterScreen
-State();
+  State<FilterScreen> createState() => _FilterScreenState();
 }
 
 class _FilterScreenState extends State<FilterScreen> {
+  var _glutenFreeFilterSet = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      drawer: MainDrawer(
+        onSelectScreen: (identifier) {
+          Navigator.of(context).pop();
+          if (identifier == 'meals') {
+            Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => const TabsScreen()));
+          }
+        },
+      ),
+      appBar: AppBar(
+        title: const Text('Your filters'),
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            title: Text(
+              'Gluten-free',
+              style: Theme.of(
+                context,
+              ).textTheme.titleLarge!.copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            subtitle: Text(
+              'Only include gluten-free meals',
+              style: Theme.of(
+                context,
+              ).textTheme.labelMedium!.copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            activeThumbColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            value: _glutenFreeFilterSet,
+            onChanged: (isChecked) {
+              setState(() {
+                _glutenFreeFilterSet = isChecked;
+              });
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:resturang_app/data/dummy_data.dart';
 import 'package:resturang_app/models/meal.dart';
 import 'package:resturang_app/screens/categories.dart';
+import 'package:resturang_app/screens/filters.dart';
 import 'package:resturang_app/screens/meals.dart';
 import 'package:resturang_app/widgets/main_drawer.dart';
 
@@ -49,9 +50,10 @@ class _TabsScreenState extends State<TabsScreen> {
   }
 
   void _setScreen(String identifier) {
+    Navigator.of(context).pop();
+
     if (identifier == 'filter') {
-    } else {
-      Navigator.of(context).pop();
+      Navigator.of(context).push(MaterialPageRoute(builder: (ctxt) => FilterScreen()));
     }
   }
 
